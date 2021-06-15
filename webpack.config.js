685d12d8634f9eb2ec7bef679cbb428b811bc8bd
gweiler <gweiler@yext.com>
2021-06-15 18:18:20 -0400

    added webpack and tailwind

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+        // Order is last to first
+            'style-loader',
+            {
+                loader: 'css-loader',
+                options: {
+                    importLoaders: 1
+                }
+            }, 
+            'postcss-loader'
+        ],
+      }
+    ],
+  }
+};
