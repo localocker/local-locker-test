758f948a4b0532425b45a5c56185c62b3f17a1d7
lymarrie <lucmarrie1@gmail.com>
2021-09-07 09:42:49 -0400

    updated font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+       'base': '1rem',
+       'lg': '1.125rem',
+       'xl': '1.25rem',
+       '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+       '5xl': '2.5rem',
+       '6xl': '4rem',
+      '7xl': '5rem',
+     },
     screens: {
       'xs': '475px',
       'sm': '640px',
