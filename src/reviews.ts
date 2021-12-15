9246834b636a36ceded1add30b085bb3b16fc4d3
lymarrie <lucmarrie1@gmail.com>
2021-12-15 08:36:39 -0500

    updated reviews logic

--- src/reviews.ts
+++ src/reviews.ts
@@ -4,11 +4,11 @@ const starSVG =
   '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>';
 
 const starRatings = {
-  ONE: starSVG.repeat(1),
-  TWO: starSVG.repeat(2),
-  THREE: starSVG.repeat(3),
-  FOUR: starSVG.repeat(4),
-  FIVE: starSVG.repeat(5),
+  1: starSVG.repeat(1),
+  2: starSVG.repeat(2),
+  3: starSVG.repeat(3),
+  4: starSVG.repeat(4),
+  5: starSVG.repeat(5),
 };
 
 const reviewsTemplate = (
@@ -67,8 +67,8 @@ const fetchReviews = async () => {
 
   reviews.forEach((r) => {
     document.getElementById("reviews-container").innerHTML += reviewsTemplate(
-      r.reviewer.author_name,
-      format(r.time.substring(0,10)),
+      r.author_name,
+      r.relative_time_description,
       starRatings[r.rating],
       r.text
     );
